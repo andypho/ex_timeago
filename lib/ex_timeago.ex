021f0931cf7f05c8c58fe3ebe794fd15db5fa840
andy <andychungho@gmail.com>
2025-06-23 09:44:12 +1000

    Init Commit

---
+++ lib/ex_timeago.ex
@@ -0,0 +1,18 @@
+defmodule ExTimeago do
+  @moduledoc """
+  Documentation for `ExTimeago`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTimeago.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_timeago_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTimeagoTest do
+  use ExUnit.Case
+  doctest ExTimeago
+
+  test "greets the world" do
+    assert ExTimeago.hello() == :world
+  end
+end
